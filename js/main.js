1b04d87904798506ec22d7b5962f4c0c3974b23e
sidrakhtar <reachsidraakhtar@gmail.com>
2022-07-04 18:58:37 -0700

    Update HTML & CSS for board and add pointers

--- js/main.js
+++ js/main.js
@@ -1,72 +1,74 @@
 /*----- constants -----*/
 const COLOR_LOOKUP = {
-   '1': 'purple',
-   '-1': 'pink',
-   null: 'white',
-};
+   // cell values: 0, 1, -1
+   '0': 'white',
+   '1': 'mediumpurple',
+   '-1': 'palevioletred', //hotpink
+}
 
-const WINNING_COMBOS = [
-   [0, 0, 0, 0]
-   [1, 1, 1, 1, 1, 1]
-   [2, 2, 2, 2, 2, 2]
-   [3, 3, 3, 3, 3, 3]
-   [4, 4, 4, 4, 4, 4]
-   [5, 5, 5, 5, 5, 5]
-   [6, 6, 6, 6, 6, 6]
-];
+// const WINNING_COMBOS = [
+//    [0, 0, 0, 0]
+//    [1, 1, 1, 1, 1, 1]
+//    [2, 2, 2, 2, 2, 2]
+//    [3, 3, 3, 3, 3, 3]
+//    [4, 4, 4, 4, 4, 4]
+//    [5, 5, 5, 5, 5, 5]
+//    [6, 6, 6, 6, 6, 6]
+// ];
 
 /*----- app's state (variables) -----*/
 // Array of  42 elements... null -> circles available; 1 or -1  for players
-let board; 
-let turn; // 1 or -1
+let board; // 2D Array wherre the nested arrays represent the columns
+let turn; // 1 or -1; 0 for empty cell
 let gameStatus; // null -> game in play; 1/-1 player win; 'T' -> tie
 // lookup how to keep track of scores. watch RPS  lecture!!!
 
 
 /*----- cached element references -----*/
-const circleEls = [...document.querySelectorAll('#board > div')];
-const msgEl = document.querySelector('h3');
+// const circleEls = [...document.querySelectorAll('#board > div')];
+// const msgEl = document.querySelector('h3');
 
 /*----- event listeners -----*/
-document.getElementById('board').addEventListener('click', handleMove);
+// document.getElementById('board').addEventListener('click', handleMove);
 
 /*----- functions -----*/
 init();
 
+// initialize state, then  calll render()
 function init() {
    // board = new Array(42).fill(null);  professor did it for ttt.. instead he used board array
    board = [
-      [null, null, null, null, null, null],
-      [null, null, null, null, null, null],
-      [null, null, null, null, null, null],
-      [null, null, null, null, null, null],
-      [null, null, null, null, null, null],
-      [null, null, null, null, null, null],
-      [null, null, null, null, null, null],
+      [0, 0, 0, 0, 0, 0], // column 0
+      [0, 0, 0, 0, 0, 0], // column 1
+      [0, 0, 0, 0, 0, 0], // column 2
+      [0, 0, 0, 0, 0, 0], // column 3
+      [0, 0, 0, 0, 0, 0], // column 4
+      [0, 0, 0, 0, 0, 0], // column 5
+      [0, 0, 0, 0, 0, 0], // column 6
    ];
    turn = 1;
-   gameStatus = null;
+   // gameStatus = null;
    render();
 }
 
 // In responce to user interaction (e.g., click)
 // We update  ALL impacted state,
 // then lastly, call render
-function handleMove(evt) {
-   // Guards
-   if (
-      gameStatus ||
-      !circleEls.includes(evt.target)
-   ) return;
-   const idx = circleEls.indexOf(evt.target);
-   board[idx] = turn;
-   turn *= -1;
-   // TODO: need to update gameStatus!!!
-   // NEED TO FIND OUT HOW TO MOVE THE CIRCLE/PLAYER'S TURN 
-   // TO LAST ITEM IN THE COLUMN!!! MAYBE CHANGE INDEX???
-   gameStatus = getGameStatus();
-   render();
-}
+// function handleMove(evt) {
+//    // Guards
+//    if (
+//       gameStatus ||
+//       !circleEls.includes(evt.target)
+//    ) return;
+//    const idx = circleEls.indexOf(evt.target);
+//    board[idx] = turn;
+//    turn *= -1;
+//    // TODO: need to update gameStatus!!!
+//    // NEED TO FIND OUT HOW TO MOVE THE CIRCLE/PLAYER'S TURN 
+//    // TO LAST ITEM IN THE COLUMN!!! MAYBE CHANGE INDEX???
+//    gameStatus = getGameStatus();
+//    render();
+// }
 
 // function getGameStatus()  {
 
@@ -75,18 +77,23 @@ function handleMove(evt) {
 
 // Render’s job is to transfer/visualize all state to the DOM
 function render() {
-   circleEls.forEach(function(circleEl, idx) {
-      circleEl.style.backgroundColor = COLOR_LOOKUP[board[idx]];
+   // Iterate over the column  arrays
+   board.forEach(function(columnArr, columnIdx) {
+      columnArr.forEach(function(cellValue, rowIdx) {
+         const cellEl = document.getElementById(`c${columnIdx}r${rowIdx}`);
+         cellEl.style.backgroundColor = COLOR_LOOKUP[cellValue];
+         console.log(cellEl, cellValue)
+      });  
    });
-   renderMessage();
+   // renderMessage();
 }
 
-function renderMessage() {
-   if (gameStatus === null) {
-      msgEl.innerHTML = `Player <span style="color: ${COLOR_LOOKUP[turn]}">${COLOR_LOOKUP[turn].toUpperCase()}</span>'s Turn`;
-   } else if (gameStatus === 'T') {
-      // Tie game
-   } else {
-      // Player has won!
-   }
-}
+// function renderMessage() {
+//    if (gameStatus === null) {
+//       msgEl.innerHTML = `Player <span style="color: ${COLOR_LOOKUP[turn]}">${COLOR_LOOKUP[turn].toUpperCase()}</span>'s Turn`;
+//    } else if (gameStatus === 'T') {
+//       // Tie game
+//    } else {
+//       // Player has won!
+//    }
+// }
