6e46b2bc058ee2febe440deca38542500ef9e90b
sidrakhtar <reachsidraakhtar@gmail.com>
2022-07-04 19:45:08 -0700

    add renderPointers & handleDrop

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 /*----- constants -----*/
-const COLOR_LOOKUP = {
+   const COLOR_LOOKUP = {
    // cell values: 0, 1, -1
    '0': 'white',
    '1': 'mediumpurple',
    '-1': 'palevioletred', //hotpink
-}
+   }
 
 // const WINNING_COMBOS = [
 //    [0, 0, 0, 0]
@@ -16,27 +16,30 @@ const COLOR_LOOKUP = {
 //    [6, 6, 6, 6, 6, 6]
 // ];
 
+
 /*----- app's state (variables) -----*/
 // Array of  42 elements... null -> circles available; 1 or -1  for players
 let board; // 2D Array wherre the nested arrays represent the columns
 let turn; // 1 or -1; 0 for empty cell
 let gameStatus; // null -> game in play; 1/-1 player win; 'T' -> tie
+
 // lookup how to keep track of scores. watch RPS  lecture!!!
 
 
 /*----- cached element references -----*/
-// const circleEls = [...document.querySelectorAll('#board > div')];
+const pointerEls = [...document.querySelectorAll('#pointers > div')];
 // const msgEl = document.querySelector('h3');
 
+
 /*----- event listeners -----*/
-// document.getElementById('board').addEventListener('click', handleMove);
+document.getElementById('pointers').addEventListener('click', handleDrop);
+
 
 /*----- functions -----*/
 init();
 
 // initialize state, then  calll render()
 function init() {
-   // board = new Array(42).fill(null);  professor did it for ttt.. instead he used board array
    board = [
       [0, 0, 0, 0, 0, 0], // column 0
       [0, 0, 0, 0, 0, 0], // column 1
@@ -54,21 +57,22 @@ function init() {
 // In responce to user interaction (e.g., click)
 // We update  ALL impacted state,
 // then lastly, call render
-// function handleMove(evt) {
-//    // Guards
-//    if (
-//       gameStatus ||
-//       !circleEls.includes(evt.target)
-//    ) return;
-//    const idx = circleEls.indexOf(evt.target);
-//    board[idx] = turn;
-//    turn *= -1;
-//    // TODO: need to update gameStatus!!!
-//    // NEED TO FIND OUT HOW TO MOVE THE CIRCLE/PLAYER'S TURN 
-//    // TO LAST ITEM IN THE COLUMN!!! MAYBE CHANGE INDEX???
-//    gameStatus = getGameStatus();
-//    render();
-// }
+function handleDrop(evt) {
+   const columnIdx = pointerEls.indexOf(evt.target);
+   // Guards
+   if (columnIdx === -1) return;
+   const columnArr = board[columnIdx];
+   if (!columnArr.includes(0)) return;
+   const rowIdx = columnArr.indexOf(0);
+   columnArr[rowIdx] = turn;
+   turn *= -1;
+
+   // TODO: need to update gameStatus!!!
+   // NEED TO FIND OUT HOW TO MOVE THE CIRCLE/PLAYER'S TURN 
+   // TO LAST ITEM IN THE COLUMN!!! MAYBE CHANGE INDEX???
+   // gameStatus = getGameStatus();
+   render();
+}
 
 // function getGameStatus()  {
 
@@ -84,6 +88,14 @@ function render() {
          cellEl.style.backgroundColor = COLOR_LOOKUP[cellValue];
          console.log(cellEl, cellValue)
       });  
+   });
+   renderPointers();
+}
+
+// to hide/show the pointers... hide if no 0' left in the column
+function renderPointers() {
+   pointerEls.forEach(function(pointerEl, columnIdx) {
+      pointerEl.style.visibility = board[columnIdx].includes(0) ? 'visible' : 'hidden';
    });
    // renderMessage();
 }
