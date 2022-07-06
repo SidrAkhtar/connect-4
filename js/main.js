eaab3e34452d5809fa583fd72223a23c94f11a04
sidrakhtar <reachsidraakhtar@gmail.com>
2022-07-06 15:49:26 -0700

    update renderMessage, winlogic, gameStatus

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 // Array of  42 elements... null -> circles available; 1 or -1  for players
 let board; // 2D Array wherre the nested arrays represent the columns
 let turn; // 1 or -1; 0 for empty cell
-let winner;
+let winner = null;
 let gameStatus; // null -> game in play; 1/-1 player win; 'T' -> tie
 
 // lookup how to keep track of scores. watch RPS  lecture!!!
@@ -44,6 +44,27 @@ function init() {
    turn = 1;
    gameStatus = null;
    render();
+   winner = null;
+}
+
+function render() {
+   // Iterate over the column  arrays
+   board.forEach(function(columnArr, columnIdx) {
+      columnArr.forEach(function(cellValue, rowIdx) {
+         const cellEl = document.getElementById(`c${columnIdx}r${rowIdx}`);
+         cellEl.style.backgroundColor = COLOR_LOOKUP[cellValue];
+         console.log(cellEl, cellValue)
+      });  
+   });
+   renderPointers();
+   renderMessage();
+}
+
+// to hide/show the pointers... hide if no 0' left in the column
+function renderPointers() {
+   pointerEls.forEach(function(pointerEl, columnIdx) {
+      pointerEl.style.visibility = board[columnIdx].includes(0) ? 'visible' : 'hidden';
+   });
 }
 
 // In responce to user interaction (e.g., click). We update ALL impacted state, then lastly, call render
@@ -62,48 +83,64 @@ function handleDrop(evt) {
    render();
 }
 
+function renderMessage() {
+   if (gameStatus === null) {
+      msgEl.innerHTML = `Player <span style="color: ${COLOR_LOOKUP[turn]}">${COLOR_LOOKUP[turn]}</span>'s Turn!`;
+   } else if (gameStatus === 'T') {
+      // Tie game
+      msgEl.textContent = 'Tie Game! Try Again!';
+  } else {
+    // Player has won!
+    msgEl.innerHTML = `Player <span style="color: ${COLOR_LOOKUP[turn*-1]}">${COLOR_LOOKUP[turn*-1]}</span>'s Wins!`;
+  }
+}
 
 function checkWin(columnIdx, rowIdx) {
    const player = board[columnIdx][rowIdx]
    return checkVertWin(columnIdx, rowIdx, player) ||
-    checkHorzWin(columnIdx, rowIdx, player)
-   // || 
-   // checkDiagWin(columnIdx, rowIdx, player);
+      checkHorzWin(columnIdx, rowIdx, player) || 
+      checkDiagWin(columnIdx, rowIdx, player);
     }
 
 function checkVertWin(columnIdx, rowIdx, player) {
-//  const columnArr = board[columnIdx];
+   const columnArr = board[columnIdx];
    let count = 1;
    // We can use/modify rowIdx because we won't need
    // to access it's original value anymore
    rowIdx--;
    // Count until no longer the same 'player'
-   while(board[columnIdx][rowIdx] === player && columnIdx >= 0) {
+   while(columnArr[rowIdx] === player && rowIdx >= 0) {
          count++;
          rowIdx--;
    }
    console.log (count);
-   return count === 4 ? winner = turn : null;
+   return count === 4 ? winner = true : null;
 }
    
 
 function checkHorzWin(columnIdx, rowIdx, player) {
+   const columnArr = board[columnIdx][rowIdx];
    console.log('columnIdx', columnIdx);
    console.log('rowIdx', columnIdx);
    let count = 1
    let idx = columnIdx + 1;
-   while((idx < board.length) && board[idx][rowIdx] === player) {
+   while((idx < board.length) && board[idx][rowIdx] === columnArr) {
       count++;
       idx--;
    }
+   idx = columnIdx -1;
+   while(idx >= 0 && board[idx][rowIdx] === columnArr) {
+      count++;
+      idx++;
+   }
       console.log (count);
-      return count >= 4 ? winner = turn : null;
+      return count >= 4 ? winner = true : null;
    }
 
 
-// function checkDiagWin(columnIdx, rowIdx, player) {
+function checkDiagWin(columnIdx, rowIdx, player) {
    
-// }
+}
 
 
 // TODO: need to update gameStatus!!!
@@ -112,40 +149,32 @@ function checkHorzWin(columnIdx, rowIdx, player) {
    // gameStatus = getGameStatus();
 
 function getGameStatus() {
-   // Player won!
-   if (winner === 1) {
-          msgEls.innerHTML = 'Purple Wins!';
-          alert('Purple Wins!')
-      } else if
-   // Player won!
-   (winner === -1) {
-      msgEls.innerHTML = 'Pink Wins!';
-    } else (winner === 'null' & !board.includes(null))
-      //Tie
-      msgEls.innerHTML = '?Tie game! Try again!'
-   }
+   let flattenBoard = board.flat(2);
+   if (!flattenBoard.includes(0)) return 'T';
+   if (winner === true) return 'W';
+   return null;
+}
 
+// function getGameStatus() {
+//    // Player won!
+//    if (winner === 1) {
+//           msgEls.innerHTML = 'Purple Wins!';
+//           alert('Purple Wins!')
+//       } else if
+//    // Player won!
+//    (winner === -1) {
+//       msgEls.innerHTML = 'Pink Wins!';
+//       alert('Pink Wins!')
+//    //  } else (winner === 'null' && !(board.includes(0)))
+//    //    //Tie
+//    //    msgEls.innerHTML = '?Tie game! Try again!'
+//    }
+// }
 
 // Render’s job is to transfer/visualize all state to the DOM
-function render() {
-   // Iterate over the column  arrays
-   board.forEach(function(columnArr, columnIdx) {
-      columnArr.forEach(function(cellValue, rowIdx) {
-         const cellEl = document.getElementById(`c${columnIdx}r${rowIdx}`);
-         cellEl.style.backgroundColor = COLOR_LOOKUP[cellValue];
-         console.log(cellEl, cellValue)
-      });  
-   });
-   renderPointers();
-}
 
-// to hide/show the pointers... hide if no 0' left in the column
-function renderPointers() {
-   pointerEls.forEach(function(pointerEl, columnIdx) {
-      pointerEl.style.visibility = board[columnIdx].includes(0) ? 'visible' : 'hidden';
-   });
-   renderMessage();
-}
+
+
 
 // function renderMessage() {
 //    if (gameStatus === null) {
@@ -160,14 +189,3 @@ function renderPointers() {
 // }
 
 
-function renderMessage() {
-   if (gameStatus === null) {
-      msgEl.innerHTML = "Next Player's turn"
-   } else if (gameStatus === 'T') {
-      // Tie game
-      msgEl.textContent = 'Tie Game! Try Again!';
-  } else {
-    // Player has won!
-    msgEl.innerHTML = `Player <span style="color: ${COLOR_LOOKUP[gameStatus]}">${COLOR_LOOKUP[gameStatus]}</span>'s Wins!`;
-  }
-}
