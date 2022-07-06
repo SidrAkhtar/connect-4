08cec79895f0c7fc8bf8a6aa055e93f45ef0d417
sidrakhtar <reachsidraakhtar@gmail.com>
2022-07-06 11:52:28 -0700

    add VertWin & HorzWin functions

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@
 // Array of  42 elements... null -> circles available; 1 or -1  for players
 let board; // 2D Array wherre the nested arrays represent the columns
 let turn; // 1 or -1; 0 for empty cell
+let winner;
 let gameStatus; // null -> game in play; 1/-1 player win; 'T' -> tie
 
 // lookup how to keep track of scores. watch RPS  lecture!!!
@@ -19,6 +20,7 @@ let gameStatus; // null -> game in play; 1/-1 player win; 'T' -> tie
 /*----- cached element references -----*/
 const pointerEls = [...document.querySelectorAll('#pointers > div')];
 const msgEl = document.querySelector('h3');
+// const msgEls = document.querySelector('h4');
 
 
 /*----- event listeners -----*/
@@ -44,9 +46,7 @@ function init() {
    render();
 }
 
-// In responce to user interaction (e.g., click)
-// We update ALL impacted state,
-// then lastly, call render
+// In responce to user interaction (e.g., click). We update ALL impacted state, then lastly, call render
 function handleDrop(evt) {
    const columnIdx = pointerEls.indexOf(evt.target);
    // Guards
@@ -56,47 +56,75 @@ function handleDrop(evt) {
    const rowIdx = columnArr.indexOf(0);
    columnArr[rowIdx] = turn;
    turn *= -1;
-
-   // TODO: need to update gameStatus!!!
-   // NEED TO FIND OUT HOW TO MOVE THE CIRCLE/PLAYER'S TURN 
-   // TO LAST ITEM IN THE COLUMN!!! MAYBE CHANGE INDEX???
-   // gameStatus = getGameStatus();
    winner = checkWin(columnIdx, rowIdx);
+   console.log(winner)
+   // gameStatus = getGameStatus();
    render();
 }
 
+
 function checkWin(columnIdx, rowIdx) {
    const player = board[columnIdx][rowIdx];
-      return checkVertWin(columnIdx, player) || checkHorzWin(columnIdx, rowIdx, player) || checkDiagWin(columnIdx, rowIdx, player);
+   if (checkVertWin(columnIdx, rowIdx, player))
+   // if (checkHorzWin(columnIdx, rowIdx, player))
+   // || 
+   // checkDiagWin(columnIdx, rowIdx, player);
+   return turn;
     }
 
 function checkVertWin(columnIdx, rowIdx, player) {
-   const colArr = board[columnIdx];
-      let count = 1;
-      // We can use/modify rowIdx because we won't need
-      // to access it's original value anymore
-      rowIdx--;
-      // Count until no longer the same 'player'
-      while(colArr[rowIdx] === player && rowIdx >= 0) {
-          count++;
-          rowIdx--;
-      }
-      return count === 4 ? player : null;
-  }
-
-
+//  const columnArr = board[columnIdx];
+   let count = 1;
+   // We can use/modify rowIdx because we won't need
+   // to access it's original value anymore
+   rowIdx--;
+   // Count until no longer the same 'player'
+   while(board[columnIdx][rowIdx] === player && columnIdx >= 0) {
+         count++;
+         rowIdx--;
+   }
+   console.log (count);
+   return count === 4 ? winner = turn : null;
+}
+   
 
 function checkHorzWin(columnIdx, rowIdx, player) {
-  
-}
+   console.log('columnIdx', columnIdx);
+   console.log('rowOdx', columnIdx);
+   let count = 1
+   let idx = columnIdx + 1;
+   while((idx < board.length) && board[idx][rowIdx] === player) {
+      count++;
+      idx--;
+   }
+      console.log (count);
+      return count >= 4 ? winner = turn : null;
+   }
 
-function checkDiagWin(columnIdx, rowIdx, player) {
+
+// function checkDiagWin(columnIdx, rowIdx, player) {
    
-}
-
-// function getGameStatus()  {
-
 // }
+
+
+// TODO: need to update gameStatus!!!
+   // NEED TO FIND OUT HOW TO MOVE THE CIRCLE/PLAYER'S TURN 
+   // TO LAST ITEM IN THE COLUMN!!! MAYBE CHANGE INDEX???
+   // gameStatus = getGameStatus();
+
+function getGameStatus() {
+   // Player won!
+   if (winner === 1) {
+          MessageEl.innerHTML = 'Purple Wins!';
+      } else if
+   // Player won!
+   (winner === -1) {
+      messageEl.innerHTML = 'Pink Wins!';
+    } else (winner === null & !board.includes(null))
+      //Tie
+      message.El.innerHTML = 'Tie game! Try again!'
+   }
+
 
 // Render’s job is to transfer/visualize all state to the DOM
 function render() {
